164c93e6756b731418c1b79856f0c38f689ce9a8
normaltech <silkcarp97@gmail.com>
2022-08-30 16:55:14 +0900

    test practice

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div className='container'>
+      <form action="">
+        <input type="text" placeholder='username'/>
+        <input type="password" placeholder='password'/>
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
